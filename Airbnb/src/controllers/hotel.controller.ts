22217d65eb9b8c644fdb67ad7b2940b79d6bd1af
Anurag07-07 <adityanv8@gmail.com>
2026-08-14 16:34:21 +0530

    Get All Hotel Router Added

--- Airbnb/src/controllers/hotel.controller.ts
+++ Airbnb/src/controllers/hotel.controller.ts
@@ -1,12 +1,13 @@
 import { NextFunction, Request, Response } from "express";
-import { createHotelService, getHotelByIdService } from "../services/hotel.service";
+import { createHotelService, getAllHotelsService, getHotelByIdService } from "../services/hotel.service";
+import { StatusCodes } from "http-status-codes";
 
 export async function createHotelHandler(req:Request,res:Response,next:NextFunction){
   //1. Call the Service layer  
     const hotel = await createHotelService(req.body);
     
   //2. Send the response
-    res.status(201).json({
+    res.status(StatusCodes.CREATED).json({
       message:`Hotel created successfully`,
       data:hotel,
       success:true
@@ -15,9 +16,32 @@ export async function createHotelHandler(req:Request,res:Response,next:NextFunct
 
 export async function getHotelByIdHandler(req:Request,res:Response,next:NextFunction) {
   const hotelResponse = await getHotelByIdService(Number(req.params.id))
-  res.status(200).json({
+  res.status(StatusCodes.OK).json({
     message:`Hotel found Successfully`,
     data:hotelResponse,
     success:true
   })
+}
+
+
+export async function getAllHotelHandler(req:Request,res:Response,next:NextFunction) {
+  //1. Call the Service Layer
+  const hotelsResponse = await getAllHotelsService();
+
+  res.status(StatusCodes.OK).json({
+    message:`Hotels found Successfully`,
+    data:hotelsResponse,
+    success:true
+  });
+}
+
+export async function deleteHotelHandler(req:Request,res:Response,next:NextFunction) {
+  res.status(StatusCodes.NOT_IMPLEMENTED).json({
+
+  });
+}
+export async function updateHotelHandler(req:Request,res:Response,next:NextFunction) {
+  res.status(StatusCodes.NOT_IMPLEMENTED).json({
+
+  });
 }
